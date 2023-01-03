af306ae1838881f23791b668ac499314e5e40715
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-01-03 14:59:19 +0000

    Extra error logic and boilerplate

--- XFEpub/mainapp/views.py
+++ XFEpub/mainapp/views.py
@@ -9,20 +9,13 @@ from django.core.exceptions import ValidationError
 def mainPage():
     return
 
-def api_webscrape_call():
-
-    
-
-    #base_url = 'https://forums.sufficientvelocity.com/threads/warhammer-fantasy-divided-loyalties-an-advisors-quest.44838/reader/'
-    #base_url = 'Fail'
-    base_url = 'https://forums.sufficientvelocity.com/threads/warhammer-fantasy-divided-loyalties-an-advisors-quest.44838'
-
+def check_url(base_url):
     val = URLValidator()
     try:
         val(base_url)
     except ValidationError:
         print("Not valid Url")
-        return
+        raise 
 
     can_continue = False
     accepted_urls = ['spacebattles.com','sufficientvelocity.com']
@@ -32,7 +25,7 @@ def api_webscrape_call():
 
     if not can_continue:
         print("Domain not accepted")
-        return
+        raise
 
     if not re.match('.*\/$', base_url):
         base_url+= '/'
@@ -40,9 +33,26 @@ def api_webscrape_call():
     if not re.match('.*\/reader\/$', base_url):
         base_url += 'reader/'
 
-    print(base_url)
+    return base_url
+
+def api_webscrape_call():
+
+    
+
+    #base_url = 'https://forums.sufficientvelocity.com/threads/warhammer-fantasy-divided-loyalties-an-advisors-quest.44838/reader/'
+    base_url = 'Fail'
+    #base_url = 'https://forums.sufficientvelocity.com/threads/warhammer-fantasy-divided-loyalties-an-advisors-quest.44838'
+    base_url = 'https://stackoverflow.com/questions/52157937/python-return-exception-from-function'
+    
+    try:
+        base_url = check_url(base_url)
+    except Exception as err:
+        
+        return # TODO Handle exception here
+
+    
+
     obj = web_scraper()
-    obj.start_boilerplate()
     obj.webscrape(base_url)
 
 class web_scraper:
@@ -61,11 +71,18 @@ class web_scraper:
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
         }
         # Fetch from user
+
+        #Test without scraping
+        soup = None
+        with open('res.txt', 'r') as f:
+            soup = bs4.BeautifulSoup(f, 'html.parser')
         
-        response = requests.get(base_url, headers)
-        self.start_boilerplate(response)
-        self.pack_articles(response)
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+        # response = requests.get(base_url, headers)
+        # soup = bs4.BeautifulSoup(response.content, 'html.parser')
+
+        self.start_boilerplate(soup)
+        self.pack_articles(soup)
+        
 
         pages_to_get = soup.find('ul', class_='pageNav-main')
         last_page = pages_to_get.find_all('li')[-1]
@@ -100,9 +117,8 @@ class web_scraper:
 
         # finish boilerstuff
 
-    def pack_articles(self, response):
+    def pack_articles(self, soup):
         '''Takes a GET Request response, and converts the chapters inside that response into chapters of the files.'''
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
         page_message_chain = soup.find('div', class_='block--messages')
         articles = page_message_chain.find_all('article', class_='message--post')
         for i in range(len(articles)):
@@ -110,7 +126,7 @@ class web_scraper:
             #The chapters title, <span class='threadmarkLabel'> articles[i].find('span', class_='threadmarkLabel')
             ##The chapter text
             with open(f'ToZip/EPUB/Chapter-{self.chapter_num}.xhtml', 'w') as f:
-                f.write(str(articles[i].find('div', class_='bbWrapper')))
+                f.write(str(articles[i].find('div', class_='bbWrapper').get_text(strip=True) ))
             self.chapter_num += 1
             pass
     
@@ -120,11 +136,21 @@ class web_scraper:
         return
 
 
-    def start_boilerplate(self, response):
+    def start_boilerplate(self, soup):
         '''Create content.opf, introduction.xhtml, nav and toc'''
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
         introduction_block = soup.find('div', class_='threadmarkListingHeader')
+        h1_tag = introduction_block.find("h1")
+        h1_tag.span.decompose()
+        
 
         with open('ToZip/EPUB/introduction.xhtml', 'w') as f:
             f.write('<?xml version=\'1.0\' encoding="utf-8"?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" epub:prefix="z3998: http://www.daisy.org/z3998/2012/vocab/structure/#" lang="en" xml:lang="en">')
-            f.write(f'<head>\n<title>{str( introduction_block.find("h1")) }</title>\n</head><body>')
+            f.write(f'<head>\n<title>{introduction_block.find("h1").get_text(strip=True) }</title>\n</head><body>')
+
+
+    def close_boilerplate():
+        '''Appends to content.opf, introduction.xhtml, nav and toc the closing tags'''
+
+
+        with open('ToZip/EPUB/introduction.xhtml', 'w') as f:
+            f.write('</body>\n</html>')
