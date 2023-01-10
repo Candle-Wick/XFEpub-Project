5f15ef44cc51edd2cd440fffb33647deecfeb3b4
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-01-10 18:03:51 +0000

    Boilerplate 0.5

--- XFEpub/mainapp/views.py
+++ XFEpub/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import requests, bs4, time, re
+import requests, bs4, time, re, os
 from pathlib import Path
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
@@ -83,7 +83,7 @@ class web_scraper:
         self.start_boilerplate(soup)
         self.pack_articles(soup)
         
-
+        # Calculates how many pages need to be fetched.
         pages_to_get = soup.find('ul', class_='pageNav-main')
         last_page = pages_to_get.find_all('li')[-1]
         last_page_number = int(last_page.find('a').text)
@@ -109,7 +109,7 @@ class web_scraper:
         return # TODO, Remove this
         
         for i in range(2, last_page_number+1):
-            print(f"https://forums.sufficientvelocity.com/threads/warhammer-fantasy-divided-loyalties-an-advisors-quest.44838/reader/page-{i}")
+            print(f"base_url{i}")
             response = requests.get(f'{base_url}page-{i}', headers)
             self.pack_articles(response)
             if i % 2 ==0:
@@ -129,6 +129,11 @@ class web_scraper:
                 f.write(" ".join(str(articles[i].find('div', class_='bbWrapper') ).split()))
             self.chapter_num += 1
             pass
+
+        #Update boilerplate
+        # nav needs new li
+        # toc needs new nav point
+        #Content needs both manifest and spine.
     
     def zip_up(self):
         #TODO
@@ -138,10 +143,48 @@ class web_scraper:
 
     def start_boilerplate(self, soup):
         '''Create content.opf, introduction.xhtml, nav and toc'''
+
+        if (not os.path.exists('ToZip/')):
+            os.makedirs('ToZip/')
+
+        if (not os.path.exists('ToZip/EPUB/')):
+            os.makedirs('ToZip/EPUB/')
+
+        if (not os.path.exists('ToZip/EPUB/style')):
+            os.makedirs('ToZip/EPUB/style')
+
+        if (not os.path.exists('ToZip/META-INF')):
+            os.makedirs('ToZip/META-INF')
+
         thread_title = soup.find('h1', class_='p-title-value')
         with open('ToZip/EPUB/introduction.xhtml', 'w') as f:
             f.write('<?xml version=\'1.0\' encoding="utf-8"?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" epub:prefix="z3998: http://www.daisy.org/z3998/2012/vocab/structure/#" lang="en" xml:lang="en">')
             f.write(f'<head>\n<title>{thread_title.get_text(strip=True) }</title>\n</head><body>')
+
+        with open('ToZip/EPUB/content.opf', 'w') as f:
+            pass
+    
+        with open('ToZip/EPUB/nav.xhtml', 'w') as f:
+            pass
+
+        with open('ToZip/EPUB/toc', 'w') as f:
+            pass
+
+        # The defaullt boilerplate, requieres nothing from what is fetched.
+        with open('ToZip/EPUB/style/main.css', 'w') as f:
+            f.write('@namespace epub "http://www.idpf.org/2007/ops";\nbody {\n    font-family: Verdana, Helvetica, Arial, sans-serif;\n}\nh1 {\n    text-align: center;\n}\nh2 {\n    text-align: left;\n    font-weight: bold;\n}\nol {\n    list-style-type: none;\n    margin: 0;\n}\nol > li {\n    margin-top: 0.3em;\n}\nol > li > span {\n    font-weight: bold;\n}\nol > li > ol {\n    margin-left: 0.5em;\n}\n.spoiler {\n    padding-left: 0.4em;\n    border-left: 0.2em solid #c7ccd1;\n}\n')
+        
+        with open('ToZip/EPUB/style/nav.css', 'w') as f:
+            f.write('BODY {color: white;}')        
+
+        with open('ToZip/EPUB/nav', 'w') as f:
+            f.write(f'<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en"><head><title>{thread_title.get_text(strip=True)}</title><link href="style/main.css" rel="stylesheet" type="text/css"/></head><body>')
+
+        with open('ToZip/mimetype', 'w') as f:
+            f.write('application/epub+zip')
+
+        with open('ToZip/META-INF', 'w') as f:
+            f.write('<?xml version="\'"1.0\' encoding=\'utf-8\'?>\n<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">\n  <rootfiles>\n    <rootfile media-type="application/oebps-package+xml" full-path="EPUB/content.opf"/>\n  </rootfiles>\n</container>')
 
 
     def close_boilerplate():
@@ -150,3 +193,12 @@ class web_scraper:
 
         with open('ToZip/EPUB/introduction.xhtml', 'w') as f:
             f.write('</body>\n</html>')
+
+        with open('ToZip/EPUB/content.opf', 'w') as f:
+            pass
+    
+        with open('ToZip/EPUB/nav.xhtml', 'w') as f:
+            pass
+
+        with open('ToZip/EPUB/toc', 'w') as f:
+            pass
