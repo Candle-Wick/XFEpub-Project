f7eae3dab87f9b70472837e4491bbe564657ee4a
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-01-18 17:36:43 +0000

    Fixed encoding and content.opf

--- XFEpub/mainapp/views.py
+++ XFEpub/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import requests, bs4, time, re, os
+import requests, bs4, time, re, os, datetime
 from pathlib import Path
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
@@ -83,11 +83,13 @@ class web_scraper:
         #     soup = bs4.BeautifulSoup(f, 'html.parser')
         
         response = requests.get(base_url, headers)
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+        response.encoding = 'utf-8'
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
         self.start_boilerplate(soup)
         self.pack_articles(soup)
 
+        self.close_boilerplate()
 
         # Calculates how many pages need to be fetched.
         pages_to_get = soup.find('ul', class_='pageNav-main')
@@ -131,20 +133,20 @@ class web_scraper:
             # Step 3. Converting each post, articles[i], into an chapter file, format.
             #The chapters title, <span class='threadmarkLabel'> articles[i].find('span', class_='threadmarkLabel')
             ##The chapter text
-            with open(f'ToZip/EPUB/Chapter-{self.chapter_num}.xhtml', 'w') as f:
+            with open(f'ToZip/EPUB/Chapter-{self.chapter_num}.xhtml', 'w', encoding='utf8') as f:
                 #TODO, write as proper html file
-                f.write(f'<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" epub:prefix="z3998: http://www.daisy.org/z3998/2012/vocab/structure/#" lang="en" xml:lang="en">\n  <head>\n    <title>{self.chapter_num}</title>\n    <link href="style/main.css" rel="stylesheet" type="text/css"/>\n  </head>\n  <body>\n    <h2>{articles[i].find("span",class_="threadmarkLabel").get_text(strip=True)}</h2>\n')
+                f.write(f'<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" epub:prefix="z3998: http://www.daisy.org/z3998/2012/vocab/structure/#" lang="en" xml:lang="en">\n  <head>\n    <title>Chapter {self.chapter_num}</title>\n    <link href="style/main.css" rel="stylesheet" type="text/css"/>\n  </head>\n  <body>\n    <h2>{articles[i].find("span",class_="threadmarkLabel").get_text(strip=True)}</h2>\n')
                 #TODO, strip of attributes
-                f.write(" ".join(str(articles[i].find('div', class_='bbWrapper') ).split()))
+                f.write(str(articles[i].find('div', class_='bbWrapper').get_text() ))
                 f.write('\n  </body>\n</html>')
             
 
             #Update boilerplate
             # nav needs new li
-            with open('ToZip/EPUB/nav.xhtml', 'a') as f:
+            with open('ToZip/EPUB/nav.xhtml', 'a', encoding='utf8') as f:
                 f.write(f'\n        <li>\n          <a href="Chapter-{self.chapter_num}.xhtml">Chapter {self.chapter_num}</a>\n        </li>')
             # toc needs new nav point
-            with open('ToZip/EPUB/toc.ncx', 'a') as f:
+            with open('ToZip/EPUB/toc.ncx', 'a', encoding='utf8') as f:
                 
                 f.write(f'<navPoint id="Chapter-{self.chapter_num}" playOrder="{self.chapter_num + 1}">\n      <navLabel>\n        <text>Chapter {self.chapter_num}</text>\n      </navLabel>\n      <content src="Chapter-{self.chapter_num}.xhtml"/>\n    </navPoint>')
             self.manifest += f'    <item href="Chapter-{self.chapter_num}.xhtml" id="chapter_{self.chapter_num}" media-type="application/xhtml+xml"/>\n'
@@ -173,43 +175,45 @@ class web_scraper:
             os.makedirs('ToZip/META-INF')
 
         thread_title = soup.find('h1', class_='p-title-value')
-        with open('ToZip/EPUB/introduction.xhtml', 'w') as f:
-            f.write('<?xml version=\'1.0\' encoding="utf-8"?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" epub:prefix="z3998: http://www.daisy.org/z3998/2012/vocab/structure/#" lang="en" xml:lang="en">')
-            f.write(f'<head>\n<title>{ thread_title.get_text(strip=True) }</title>\n</head><body>')
-
         thread_description = soup.find('article', class_='threadmarkListingHeader-extraInfoChild')
         creator = soup.find('a', class_='username').get_text()
-        with open('ToZip/EPUB/content.opf', 'w') as f:
-            #TODO, add proper modified syntax, <time datetime='X'>, strip and take left of +
-            #TODO, add Identifier, derive from timestamp + something?
+        datetime_now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ%Z")
+        #Given that there is no UUID, DOI or ISBN typically associated with the content scraped. A hash is instead generated for a identifier that will likely be unique.
+        identifier = hash(thread_title.get_text()+creator+datetime_now)
+        with open('ToZip/EPUB/introduction.xhtml', 'w', encoding='utf8') as f:
+            f.write('<?xml version=\'1.0\' encoding="utf-8"?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" epub:prefix="z3998: http://www.daisy.org/z3998/2012/vocab/structure/#" lang="en" xml:lang="en">')
+            f.write(f'<head>\n<title>{ thread_title.get_text(strip=True) }</title>\n</head><body>')
+            #TODO, add the rest to this
+
+        with open('ToZip/EPUB/content.opf', 'w', encoding='utf8') as f:
             f.write(f'<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="id" version="3.0" prefix="rendition: http://www.idpf.org/vocab/rendition/#">')
-            f.write(f'\n  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">\n    <meta property="dcterms:modified">2023-01-17T18:59:52z{""}</meta>\n    <meta name="generator" content="Ebook-lib 0.17.1"/>')
-            f.write(f'\n    <dc:identifier id="id">{2}</dc:identifier>\n    <dc:title>{thread_title.get_text(strip=True) }</dc:title>\n    <dc:language>en</dc:language>\n    <dc:creator id="creator">{creator}</dc:creator>\n    <dc:description>{thread_description.get_text(strip=True)}</dc:description>\n  </metadata>')
+            f.write(f'\n  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">\n    <meta property="dcterms:modified">{datetime_now}</meta>\n    <meta name="generator" content="Ebook-lib 0.17.1"/>')
+            f.write(f'\n    <dc:identifier id="id">{identifier}</dc:identifier>\n    <dc:title>{thread_title.get_text(strip=True) }</dc:title>\n    <dc:language>en</dc:language>\n    <dc:creator id="creator">{creator}</dc:creator>\n    <dc:description>{thread_description.get_text(strip=True)}</dc:description>\n  </metadata>')
             f.write(f'\n  <manifest>\n    <item href="style/main.css" id="doc_style" media-type="text/css"/>\n    <item href="style/nav.css" id="style_nav" media-type="text/css"/>\n    <item href="introduction.xhtml" id="chapter_0" media-type="application/xhtml+xml"/>\n    <item href="nav.xhtml" id="nav" media-type="application/xhtml+xml" properties="nav"/>')
 
-        with open('ToZip/EPUB/nav.xhtml', 'w') as f:
+        with open('ToZip/EPUB/nav.xhtml', 'w', encoding='utf8') as f:
             f.write(f'<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">\n  <head>\n    <title>{thread_title.get_text(strip=True) }</title>\n    <link href="style/main.css" rel="stylesheet" type="text/css"/>\n  </head>\n  <body>\n    <nav epub:type="toc" id="id" role="doc-toc">\n      <h2>{thread_title.get_text(strip=True) }</h2>')
             f.write(f'<li>\n          <a href="introduction.xhtml">Introduction</a>\n        </li>')
 
-        with open('ToZip/EPUB/toc.ncx', 'w') as f:
+        with open('ToZip/EPUB/toc.ncx', 'w', encoding='utf8') as f:
             f.write(f'<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">\n  <head>\n    <meta content="ot4c29vn" name="dtb:uid"/>\n    <meta content="0" name="dtb:depth"/>\n    <meta content="0" name="dtb:totalPageCount"/>\n    <meta content="0" name="dtb:maxPageNumber"/>\n  </head>\n  <docTitle>\n    <text>{thread_title.get_text(strip=True)}</text>\n  </docTitle>\n  <navMap>')
             f.write(f'    <navPoint id="intro" playOrder="1">\n     <navLabel>\n       <text>Introduction</text>\n     </navLabel>\n     <content src="introduction.xhtml"/>\n   </navPoint>')
 
 
-        # The defaullt boilerplate, requieres nothing from what is fetched.
-        with open('ToZip/EPUB/style/main.css', 'w') as f:
+        # The defaullt boilerplate, requires nothing from what is fetched.
+        with open('ToZip/EPUB/style/main.css', 'w', encoding='utf8') as f:
             f.write('@namespace epub "http://www.idpf.org/2007/ops";\nbody {\n    font-family: Verdana, Helvetica, Arial, sans-serif;\n}\nh1 {\n    text-align: center;\n}\nh2 {\n    text-align: left;\n    font-weight: bold;\n}\nol {\n    list-style-type: none;\n    margin: 0;\n}\nol > li {\n    margin-top: 0.3em;\n}\nol > li > span {\n    font-weight: bold;\n}\nol > li > ol {\n    margin-left: 0.5em;\n}\n.spoiler {\n    padding-left: 0.4em;\n    border-left: 0.2em solid #c7ccd1;\n}\n')
         
-        with open('ToZip/EPUB/style/nav.css', 'w') as f:
+        with open('ToZip/EPUB/style/nav.css', 'w', encoding='utf8') as f:
             f.write('BODY {color: white;}')        
 
-        with open('ToZip/EPUB/nav.xhtml', 'w') as f:
+        with open('ToZip/EPUB/nav.xhtml', 'w', encoding='utf8') as f:
             f.write(f'<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<!DOCTYPE html>\n<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">\n  <head>\n    <title>{thread_title.get_text(strip=True)}</title>\n    <link href="style/main.css" rel="stylesheet" type="text/css"/>\n  </head>\n  <body>\n    <nav epub:type="toc" id="id" role="doc-toc">\n      <h2>{thread_title.get_text(strip=True)}</h2>\n      <ol>')
 
-        with open('ToZip/mimetype', 'w') as f:
+        with open('ToZip/mimetype', 'w', encoding='utf8') as f:
             f.write('application/epub+zip')
 
-        with open('ToZip/META-INF/container.xml', 'w') as f:
+        with open('ToZip/META-INF/container.xml', 'w', encoding='utf8') as f:
             f.write('<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">\n  <rootfiles>\n    <rootfile media-type="application/oebps-package+xml" full-path="EPUB/content.opf"/>\n  </rootfiles>\n</container>')
 
 
@@ -217,17 +221,17 @@ class web_scraper:
         '''Appends to content.opf, introduction.xhtml, nav and toc the closing tags'''
 
 
-        with open('ToZip/EPUB/introduction.xhtml', 'a') as f:
+        with open('ToZip/EPUB/introduction.xhtml', 'a', encoding='utf8') as f:
             f.write('</body>\n</html>')
 
-        with open('ToZip/EPUB/content.opf', 'a') as f:
+        with open('ToZip/EPUB/content.opf', 'a', encoding='utf8') as f:
             f.write(self.manifest)
             f.write('    <item href="toc.ncx" id="ncx" media-type="application/x-dtbncx+xml"/>\n  </manifest>\n  <spine toc="ncx">')
             f.write(self.spine)
             f.write('  </spine>\n</package>')   
 
-        with open('ToZip/EPUB/nav.xhtml', 'a') as f:
+        with open('ToZip/EPUB/nav.xhtml', 'a', encoding='utf8') as f:
             f.write('\n      </ol>\n    </nav>\n  </body>\n</html>')
 
-        with open('ToZip/EPUB/toc.ncx', 'a') as f:
+        with open('ToZip/EPUB/toc.ncx', 'a', encoding='utf8') as f:
             f.write('\n  </navMap>\n</ncx>')
