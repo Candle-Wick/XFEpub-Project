e7c05e3adb0e949d36481d4e880b0c0f4a685e5d
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-01-24 15:25:35 +0000

    build settings fiddling

--- XFEpub/XFEpub/urls.py
+++ XFEpub/XFEpub/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from mainapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls'))
+    path('', mainPage)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
