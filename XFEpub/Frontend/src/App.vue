b4f4235fa330f4fb74cc12d6d2a603a5bc9e7381
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-01-29 13:15:37 +0000

    vue foundation and planning. v0.5

--- XFEpub/Frontend/src/App.vue
+++ XFEpub/Frontend/src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import mainBody from './components/main.vue'
+
+
 </script>
 
 <template>
-  <header>
+
+  <mainBody class=""/>
+
+
+
+  <!-- <header>
+
+    
+
+  
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,10 +26,10 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> 
   </header>
 
-  <RouterView />
+  <RouterView /> -->
 </template>
 
 <style scoped>
---
+++ XFEpub/Frontend/src/components/main.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <div class="">
+        <form action="">
+            <label for="thread_url" class="form-label">Choose a thread to download:</label>
+            <input type="url" id="thread_url" name="thread_url" class="form-control">
+
+
+
+        </form>
+    </div>
+</template>
+
+<script>
+// A script that will hide the toggles when there is no URL in thread_url
+
+// A script taht will send off the url packet
+
+
+</script>
+
+<style>
+.hidden {
+    display: none;
+}
+
+</style>
