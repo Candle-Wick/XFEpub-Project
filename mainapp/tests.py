107c47f79e432072eeb9aee46eed9deb6536450e
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-03-02 14:33:28 +0000

    deployment v0.4

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -2,6 +2,4 @@ from django.test import TestCase
 from mainapp.views import api_webscrape_call
 import timeit
 # Create your tests here.
-class webTestCase(TestCase):
-    def test(self):
-        print(timeit.timeit(api_webscrape_call, number=1))
+
