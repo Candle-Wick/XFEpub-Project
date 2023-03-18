54be5981c33b7563c6013f92827a0fa1eacaa9a7
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-03-18 14:02:13 +0000

    Removing some leftovers

--- Epubs/views.py
+++ Epubs/views.py
@@ -6,11 +6,7 @@ def epub_view(request, epub_path):
         return
     with open('Epubs/'+epub_path, 'rb') as f:
         file = f.read()
-        #Delete file?
     response = HttpResponse(file, 'application/epub+zip')
-    #response['Content-Length'] = file.size
     response['Content-Disposition'] = f'attachment; filename="{epub_path}"'
-
-
     return response
 
