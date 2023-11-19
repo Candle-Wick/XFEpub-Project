192845be8dffaf71fda5d11abdb6a1f38b043885
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2023-11-19 18:41:14 +0000

    Converting to command line.

--- XFEpub/mainapp/error.py
+++
@@ -1,5 +0,0 @@
-class BadRequestException(Exception):
-
-    def __init__(self, error_type: str, *args: object) -> None:
-        self.error_type = error_type 
-        super().__init__(*args)
--- XFEpub/mainapp/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from mainapp.views import api_webscrape_call
-import timeit
-# Create your tests here.
-class webTestCase(TestCase):
-    def test(self):
-        print(timeit.timeit(api_webscrape_call, number=1))
